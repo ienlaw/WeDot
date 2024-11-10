f0266d46c9917f281f3b5a5292a17170076c25ba
PJ568 <PJ568@PJ568.eu.org>
2024-11-10 11:52:00 +0800

    【新增】隐藏窗口模式

--- platform/windows/display_server_windows.h
+++ platform/windows/display_server_windows.h
@@ -38,7 +38,6 @@
 
 #include "core/config/project_settings.h"
 #include "core/input/input.h"
-#include "core/io/image.h"
 #include "core/os/os.h"
 #include "drivers/unix/ip_unix.h"
 #include "drivers/wasapi/audio_driver_wasapi.h"
@@ -357,12 +356,8 @@ typedef enum _SHC_PROCESS_DPI_AWARENESS {
 	SHC_PROCESS_PER_MONITOR_DPI_AWARE = 2,
 } SHC_PROCESS_DPI_AWARENESS;
 
-class DropTargetWindows;
-
 class DisplayServerWindows : public DisplayServer {
 	// No need to register with GDCLASS, it's platform-specific and nothing is added.
-
-	friend class DropTargetWindows;
 
 	_THREAD_SAFE_CLASS_
 
@@ -398,14 +393,6 @@ class DisplayServerWindows : public DisplayServer {
 	void _update_tablet_ctx(const String &p_old_driver, const String &p_new_driver);
 	String tablet_driver;
 	Vector<String> tablet_drivers;
-
-	enum DriverID {
-		DRIVER_ID_COMPAT_OPENGL3 = 1 << 0,
-		DRIVER_ID_COMPAT_ANGLE_D3D11 = 1 << 1,
-		DRIVER_ID_RD_VULKAN = 1 << 2,
-		DRIVER_ID_RD_D3D12 = 1 << 3,
-	};
-	static BitField<DriverID> tested_drivers;
 
 	enum TimerID {
 		TIMER_ID_MOVE_REDRAW = 1,
@@ -478,7 +465,7 @@ class DisplayServerWindows : public DisplayServer {
 		bool exclusive = false;
 		bool context_created = false;
 		bool mpass = false;
-		bool sharp_corners = false;
+		bool visible = true;
 
 		// Used to transfer data between events using timer.
 		WPARAM saved_wparam;
@@ -525,16 +512,11 @@ class DisplayServerWindows : public DisplayServer {
 		Callable input_text_callback;
 		Callable drop_files_callback;
 
-		// OLE API
-		DropTargetWindows *drop_target = nullptr;
-
 		WindowID transient_parent = INVALID_WINDOW_ID;
 		HashSet<WindowID> transient_children;
 
 		bool is_popup = false;
 		Rect2i parent_safe_rect;
-
-		bool initialized = false;
 	};
 
 	JoypadWindows *joypad = nullptr;
@@ -602,7 +584,7 @@ class DisplayServerWindows : public DisplayServer {
 	HashMap<int64_t, Vector2> pointer_last_pos;
 
 	void _send_window_event(const WindowData &wd, WindowEvent p_event);
-	void _get_window_style(bool p_main_window, bool p_initialized, bool p_fullscreen, bool p_multiwindow_fs, bool p_borderless, bool p_resizable, bool p_minimized, bool p_maximized, bool p_maximized_fs, bool p_no_activate_focus, DWORD &r_style, DWORD &r_style_ex);
+	void _get_window_style(bool p_main_window, bool p_fullscreen, bool p_multiwindow_fs, bool p_borderless, bool p_resizable, bool p_maximized, bool p_maximized_fs, bool p_no_activate_focus, DWORD &r_style, DWORD &r_style_ex);
 
 	MouseMode mouse_mode;
 	int restore_mouse_trails = 0;
